8f79f9dc00ed0102769b1808f6895924036263cf
Amir Mohammadi <amirkamangar@icloud.com>
2023-01-05 00:24:42 +0330

    Update PatreonAPI.swift

--- DecryptKit/Functions/PatreonAPI.swift
+++ DecryptKit/Functions/PatreonAPI.swift
@@ -7,10 +7,19 @@
 
 import Foundation
 
-struct PatreonData {
+struct PatreonClient {
   let clientID = "MKzRZxagIOea-ceFt_54sjf9yyA2TzTHln9LiUoybU8ZRg7ljS4KE9HrBPa9i6aA"
   let clientSecret = "4kTPw037oTE6D9zGHDwBgxqljtd70UkLBXAA25lIk83ZRbrlQjNr3sN8-TFa8dI6"
-  let accessToken = "3YVSAbYWvmtWCEmVItQaXjZGyHqYgGy8_w4mVV_47h4"
-  let refreshToken = "NjmDj3mZ1b5ae7zcNKZD6cmZRtfsEdddCnRvaywliL4"
+  let creatorAccessToken = "3YVSAbYWvmtWCEmVItQaXjZGyHqYgGy8_w4mVV_47h4"
+  let creatorRefreshToken = "NjmDj3mZ1b5ae7zcNKZD6cmZRtfsEdddCnRvaywliL4"
   let campaignID = "9760149"
+}
+
+class Patreon {
+  let client = PatreonClient()
+  let redirectURL = "deCripple://patreon?"
+  
+  func oauth() {
+    
+  }
 }
