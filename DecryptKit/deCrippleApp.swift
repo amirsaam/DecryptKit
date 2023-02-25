6bf2847b19187f5092c4fb796fa1810ae098836d
Amir Mohammadi <amirkamangar@icloud.com>
2023-02-25 18:19:27 +0330

    PlayCover URL Scheme

--- DecryptKit/deCrippleApp.swift
+++ DecryptKit/deCrippleApp.swift
@@ -16,7 +16,7 @@ let mainColor = Color.Neumorphic.main
 let secondaryColor = Color.Neumorphic.secondary
 let defaults = UserDefaults.standard
 let cache = DataCache.instance
-let playcoverURL = URL(string: "playcover:source?action=add&url=https://amrsm.ir/decrypted.json")
+let playcoverURL = URL(string: "apple-magnifier://source?action=add&url=https://amrsm.ir/decrypted.json")
 let patreonAPI = PatreonAPI(clientID: PatreonClient.shared.clientID,
                             clientSecret: PatreonClient.shared.clientSecret,
                             creatorAccessToken: PatreonClient.shared.creatorAccessToken,
